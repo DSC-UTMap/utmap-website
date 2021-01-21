53c71290a21bc0fec860b75b6f0a2bf9cba51566
Leila-U <leila.uy@mail.utoronto.ca>
2021-01-20 22:40:58 -0500

    Add calendar component

--- utmap-client/src/App.js
+++ utmap-client/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//import { render } from "react-dom";
+import BigCalendar from './components/BigCalendar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  //example events REMOVE once the backend is connected
+  state = {
+    events: [
+      {
+        id: 0,
+        title: 'First Event',
+        start: new Date('2021-01-23 00:00'),
+        end: new Date('2021-01-23 01:00'),
+      },
+      {
+        id: 1,
+        title: 'Second Event',
+        start: new Date('2021-01-30 00:00'),
+        end: new Date('2021-01-31 01:00'),
+      },
+      {
+        id: 2,
+        title: 'Third Event',
+        start: new Date('2021-03-01 00:00'),
+        end: new Date('2021-03-01 15:00'),
+      }
+    ]
+  }
+
+  render(){
+    return(
+      //sends the events from state to our BigCalendar
+      <div className="container">
+        <BigCalendar events={this.state.events} />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ utmap-client/src/components/BigCalendar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from 'moment';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+moment.locale("en-CA");
+const localizer = momentLocalizer(moment);
+
+class BigCalendar extends Component {
+    getStyle = () => {
+        return {
+            height: 700, //needs a strict height or flexbox to show month calendar
+            padding: '20px',
+        }
+    }
+    render() {
+        return (
+            <div style={this.getStyle()}>
+                <Calendar 
+                    localizer={localizer} 
+                    defaultView='month'
+                    step={60}
+                    events={this.props.events} 
+                    startAccessor="start" 
+                    endAccessor="end" 
+                    showMultiDayTimes  
+                />
+            </div>
+        );
+    }
+}
+
+export default BigCalendar
