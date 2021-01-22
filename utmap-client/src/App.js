82f73f5769cf949262015d1a9eda1488217673bf
Leila-U <leila.uy@mail.utoronto.ca>
2021-01-22 15:34:40 -0500

    Fix calendar page and app state

--- utmap-client/src/App.js
+++ utmap-client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-//import { render } from "react-dom";
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import LandingPage from './components/pages/LandingPage';
 import CalendarPage from './components/pages/CalendarPage';
@@ -7,29 +6,13 @@ import MapPage from './components/pages/MapPage';
 
 
 class App extends Component{
-  //example events REMOVE once the backend is connected
-  state = {
-    events: [
-      { startDate: '2021-01-03T09:45', endDate: '2021-01-03T10:00', title: 'Cry and Code' },
-      { startDate: '2021-01-27T12:00', endDate: '2021-01-29T13:30', title: 'Attend Andis Awesome Lecture' },
-      { startDate: '2021-03-27T12:00', endDate: '2021-03-30T13:30', title: 'Get jacked' }
-    ]
-  }
-
   render(){
     return(
       //Landing, Calendar, and Map Page
       <Router>
         <div className="App">
           <Route exact path="/" component={LandingPage}/>    
-          <Route 
-            path="/calendar" 
-            render={props => (
-              <React.Fragment>
-                <CalendarPage events={this.state.events} />
-              </React.Fragment>
-            )} 
-          />
+          <Route path="/calendar" component={CalendarPage}/>
           <Route path="/map" component={MapPage}/>
           <div><Link to="/">Landing</Link></div>
           <div><Link to="/calendar">Calendar</Link></div>
--- utmap-client/src/components/pages/CalendarPage.js
+++ utmap-client/src/components/pages/CalendarPage.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { Component } from "react";
 import Paper from '@material-ui/core/Paper';
 import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
@@ -11,19 +11,34 @@ import {
 } from '@devexpress/dx-react-scheduler-material-ui';
 
 //Scheduler is the calendar, today, and taskbar components
-function CalendarPage(props) {
-    return (
-        <Paper>
-            <Scheduler data={props.events}>
-            <ViewState />
-            <MonthView />
-            <Toolbar />
-            <DateNavigator />
-            <TodayButton />
-            <Appointments />
-            </Scheduler>
-        </Paper>
-    );
+class CalendarPage extends Component {
+    //example events REMOVE once the backend is connected
+    state = {
+        events: [
+            { startDate: '2021-01-03T09:45', endDate: '2021-01-03T10:00', title: 'Cry and Code' },
+            { startDate: '2021-01-27T12:00', endDate: '2021-01-29T13:30', title: 'Attend Andis Awesome Lecture' },
+            { startDate: '2021-03-27T12:00', endDate: '2021-03-30T13:30', title: 'Get jacked' }
+        ]
+    }
+
+    render () {
+        const { events } = this.state;
+        return (
+            <Paper>
+                <Scheduler data={events}>
+                <ViewState />
+                <MonthView />
+                <Toolbar />
+                <DateNavigator />
+                <TodayButton />
+                <Appointments />
+                </Scheduler>
+            </Paper>
+        );
+    }
+    
+
+    
 }
 
 export default CalendarPage;
