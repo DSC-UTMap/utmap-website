5205e6faab381226709836f099a7139b564c8c81
Leila-U <leila.uy@mail.utoronto.ca>
2021-01-22 17:05:59 -0500

    Change calendar and app to functional components

--- utmap-client/src/App.js
+++ utmap-client/src/App.js
@@ -1,27 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import LandingPage from './components/pages/LandingPage';
 import CalendarPage from './components/pages/CalendarPage';
 import MapPage from './components/pages/MapPage';
 
-
-class App extends Component{
-  render(){
-    return(
-      //Landing, Calendar, and Map Page
-      <Router>
-        <div className="App">
-          <Route exact path="/" component={LandingPage}/>    
-          <Route path="/calendar" component={CalendarPage}/>
-          <Route path="/map" component={MapPage}/>
-          <div><Link to="/">Landing</Link></div>
-          <div><Link to="/calendar">Calendar</Link></div>
-          <div><Link to="/map">Map</Link></div>
-        </div>
-     </Router>
-    )
-  }
+function App() {
+  return(
+    //Landing, Calendar, and Map Page
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={LandingPage}/>    
+        <Route path="/calendar" component={CalendarPage}/>
+        <Route path="/map" component={MapPage}/>
+        <div><Link to="/">Landing</Link></div>
+        <div><Link to="/calendar">Calendar</Link></div>
+        <div><Link to="/map">Map</Link></div>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
-
--- utmap-client/src/components/pages/CalendarPage.js
+++ utmap-client/src/components/pages/CalendarPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Paper from '@material-ui/core/Paper';
 import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
@@ -10,35 +10,27 @@ import {
   TodayButton,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
+//example events REMOVE once the backend is connected
+const exampleEvents = [
+    { startDate: '2021-01-03T09:45', endDate: '2021-01-03T10:00', title: 'Cry and Code' },
+    { startDate: '2021-01-27T12:00', endDate: '2021-01-29T13:30', title: 'Attend Andis Awesome Lecture' },
+    { startDate: '2021-03-27T12:00', endDate: '2021-03-30T13:30', title: 'Get jacked' }
+]
+
 //Scheduler is the calendar, today, and taskbar components
-class CalendarPage extends Component {
-    //example events REMOVE once the backend is connected
-    state = {
-        events: [
-            { startDate: '2021-01-03T09:45', endDate: '2021-01-03T10:00', title: 'Cry and Code' },
-            { startDate: '2021-01-27T12:00', endDate: '2021-01-29T13:30', title: 'Attend Andis Awesome Lecture' },
-            { startDate: '2021-03-27T12:00', endDate: '2021-03-30T13:30', title: 'Get jacked' }
-        ]
-    }
-
-    render () {
-        const { events } = this.state;
-        return (
-            <Paper>
-                <Scheduler data={events}>
-                <ViewState />
-                <MonthView />
-                <Toolbar />
-                <DateNavigator />
-                <TodayButton />
-                <Appointments />
-                </Scheduler>
-            </Paper>
-        );
-    }
-    
-
-    
+ function CalendarPage() {    
+    return (
+        <Paper>
+            <Scheduler data={exampleEvents}>
+            <ViewState />
+            <MonthView />
+            <Toolbar />
+            <DateNavigator />
+            <TodayButton />
+            <Appointments />
+            </Scheduler>
+        </Paper>
+    );    
 }
 
 export default CalendarPage;
