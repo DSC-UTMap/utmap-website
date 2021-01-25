492e1d392d81acceb4fa88020d9f17ff6e568d1c
Kana Hashimoto <kana.hashimoto@mail.utoronto.ca>
2021-01-25 11:01:23 -0500

    Changed indentation to tabs

--- utmap-client/src/components/pages/CreateEventPage.js
+++ utmap-client/src/components/pages/CreateEventPage.js
@@ -17,154 +17,154 @@ import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({ //CSS styles on components
-  box: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'gray',
-  },
-  paper: {
-    width: '40rem',
-    marginTop: theme.spacing(6),
-    paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(6),
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
-    display: 'flex',
-  },
-  row: {
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-  },
-  longBox: {
-    width: '100%',
-    marginLeft: theme.spacing(2),
-  },
-  shortBox: {
-    width: '50%',
-    marginLeft: theme.spacing(2),
-  },
+	box: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'gray',
+	},
+	paper: {
+		width: '40rem',
+		marginTop: theme.spacing(6),
+		paddingTop: theme.spacing(4),
+		paddingBottom: theme.spacing(6),
+		paddingLeft: theme.spacing(2),
+		paddingRight: theme.spacing(2),
+		display: 'flex',
+	},
+	row: {
+		width: '100%',
+		display: 'flex',
+		alignItems: 'center',
+	},
+	longBox: {
+		width: '100%',
+		marginLeft: theme.spacing(2),
+	},
+	shortBox: {
+		width: '50%',
+		marginLeft: theme.spacing(2),
+	},
 }));
 
 
 function CreateEventPage() {
-  const formStyle = useStyles(); 
-  const [selectedDate, handleDateChange] = useState(new Date());
-  const [location, setLocation] = useState('SC');
-  let history = useHistory(); //For Router redirection
+	const formStyle = useStyles(); 
+	const [selectedDate, handleDateChange] = useState(new Date());
+	const [location, setLocation] = useState('SC');
+	let history = useHistory(); //For Router redirection
 
-  const handleLocationChange = event => setLocation(event.target.value);
-  //This is where the form will send to server
-  const handleSubmit = event => {
-    event.preventDefault(); //Stop the form from submitting
-    history.push('/'); //Redirect to this page
-  }
-  return (
-    <div className={formStyle.box}>
-      <Paper className={formStyle.paper} >
-        <form className='eventForm' action='' method='post' onSubmit={handleSubmit}>
-        <Grid container spacing={3}>
-          <Grid item className={formStyle.row}>
+	const handleLocationChange = event => setLocation(event.target.value);
+	//This is where the form will send to server
+	const handleSubmit = event => {
+		event.preventDefault(); //Stop the form from submitting
+		history.push('/'); //Redirect to this page
+	}
+	return (
+		<div className={formStyle.box}>
+		<Paper className={formStyle.paper} >
+		<form className='eventForm' action='' method='post' onSubmit={handleSubmit}>
+		<Grid container spacing={3}>
+ 			<Grid item className={formStyle.row}>
 
-            <CreateIcon/>
-            <TextField
-              className={formStyle.longBox}
-              id='eventTitle'
-              variant='outlined'
-              name='title'
-              label='Title'
-              required
-              autofocus
-            />
-          </Grid>
-
-
-          <Grid item className={formStyle.row}>
-            <CalendarTodayIcon/>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-              <DateTimePicker 
-                className={formStyle.shortBox}
-                name='startDate'
-                value={selectedDate}
-                onChange={handleDateChange} 
-                inputVariant='outlined'
-                label='Start Date'
-                disablePast
-              />
-              <DateTimePicker 
-                className={formStyle.shortBox}
-                name='endDate'
-                value={selectedDate}
-                onChange={handleDateChange} 
-                inputVariant='outlined'
-                label='End Date'
-                disablePast
-              />
-            </MuiPickersUtilsProvider>
-          </Grid>
+				<CreateIcon/>
+	            <TextField
+					className={formStyle.longBox}
+					id='eventTitle'
+					variant='outlined'
+					name='title'
+					label='Title'
+					required
+					autofocus
+	            />
+			</Grid>
 
 
-          <Grid item className={formStyle.row}>
-            <AddLocationIcon/>
-            <FormControl 
-              className={formStyle.shortBox} 
-              variant='outlined' 
-              required
-            >
-              <InputLabel>Location</InputLabel>
-              <Select
-                id='eventLocation'
-                name='location'
-                value={location}
-                onChange={handleLocationChange}
-                label='Location *'
-              >
-                <MenuItem value='SC'>Student Center</MenuItem>
-                <MenuItem value='DV'>Davis (DV)</MenuItem>
-                <MenuItem value='IB'>Instructional Building (IB)</MenuItem>
-                <MenuItem value='DH'>Deerfield Hall (DH)</MenuItem>
-                <MenuItem value='MN'>Maanjiwe Nendamowinan (MN)</MenuItem>
-              </Select>
-            </FormControl>
-            <TextField
-              className={formStyle.shortBox}
-              id='eventSublocation'
-              name='sublocation'
-              variant='outlined'
-              label='Room'
-            />
-          </Grid>
+			<Grid item className={formStyle.row}>
+				<CalendarTodayIcon/>
+				<MuiPickersUtilsProvider utils={DateFnsUtils}>
+ 					<DateTimePicker 
+						className={formStyle.shortBox}
+						name='startDate'
+						value={selectedDate}
+						onChange={handleDateChange} 
+						inputVariant='outlined'
+						label='Start Date'
+						disablePast
+					/>
+					<DateTimePicker 
+						className={formStyle.shortBox}
+						name='endDate'
+						value={selectedDate}
+						onChange={handleDateChange} 
+						inputVariant='outlined'
+						label='End Date'
+						disablePast
+					/>
+				</MuiPickersUtilsProvider>
+			</Grid>
 
 
-          <Grid item className={formStyle.row}>
-            <DescriptionIcon/>
-            <TextField
-              className={formStyle.longBox}
-              id="eventDescription"
-              name='description'
-              label='Description'
-              multiline
-              rows={4}
-              variant='outlined'
-            />
-          </Grid>
+			<Grid item className={formStyle.row}>
+				<AddLocationIcon/>
+				<FormControl 
+					className={formStyle.shortBox} 
+					variant='outlined' 
+					required
+				>
+					<InputLabel>Location</InputLabel>
+					<Select
+						id='eventLocation'
+						name='location'
+						value={location}
+						onChange={handleLocationChange}
+						label='Location *'
+					>
+						<MenuItem value='SC'>Student Center</MenuItem>
+						<MenuItem value='DV'>Davis (DV)</MenuItem>
+						<MenuItem value='IB'>Instructional Building (IB)</MenuItem>
+						<MenuItem value='DH'>Deerfield Hall (DH)</MenuItem>
+						<MenuItem value='MN'>Maanjiwe Nendamowinan (MN)</MenuItem>
+					</Select>
+				</FormControl>
+				<TextField
+					className={formStyle.shortBox}
+					id='eventSublocation'
+					name='sublocation'
+					variant='outlined'
+					label='Room'
+				/>
+			</Grid>
 
 
-          <Grid item className={formStyle.row} justify='flex-end'>
-            <Button 
-              variant='contained' 
-              color='primary'
-              type='submit'
-            >
-              Submit
-            </Button>
-          </Grid>
-        </Grid>
-        </form>
-      </Paper>
-    </div>
-  );
+			<Grid item className={formStyle.row}>
+				<DescriptionIcon/>
+				<TextField
+					className={formStyle.longBox}
+					id="eventDescription"
+					name='description'
+					label='Description'
+					multiline
+					rows={4}
+					variant='outlined'
+				/>
+			</Grid>
+
+
+			<Grid item className={formStyle.row} justify='flex-end'>
+				<Button 
+					variant='contained' 
+					color='primary'
+					type='submit'
+				>
+					Submit
+				</Button>
+			</Grid>
+		</Grid>
+		</form>
+		</Paper>
+    	</div>
+	);
 }
 
 export default CreateEventPage;
