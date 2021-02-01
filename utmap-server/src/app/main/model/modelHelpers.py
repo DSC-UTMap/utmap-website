3b13e940ce76aca66b75d9517387aca7be40c960
saniirob <roberto.sanii@mail.utoronto.ca>
2021-02-01 17:08:44 -0500

    Implement more helper functions for model

--- utmap-server/src/app/main/model/modelHelpers.py
+++ utmap-server/src/app/main/model/modelHelpers.py
@@ -1,16 +1,24 @@
 from bson import ObjectId
 
+def findAll(collection):
+    return collection.find()
+
 def findById(_id, collection):
-    doc = collection.find_one({'id' : ObjectId(_id)})
+    doc = collection.find_one({'_id' : ObjectId(_id)})
+    return doc
+
+def deleteById(_id, collection):
+    doc = collection.find_one_and_delete({'_id' : ObjectId(_id)})
     return doc
 
 def findByName(name, collection):
     doc = collection.find_one({'name' : str(name)})
     return doc
 
-def formatSubCollection(collection):
-    #for document in
-    pass
+def formatDocuments(documentList):
+    for doc in documentList:
+        doc['_id'] = formatId(doc['_id'])
+    return documentList
 
 def formatId(_id):
     return str(_id)
