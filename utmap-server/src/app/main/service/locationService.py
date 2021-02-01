fa6799adf0ca4394f162e7af7158c62fb45c56e2
saniirob <roberto.sanii@mail.utoronto.ca>
2021-02-01 17:07:47 -0500

    Add to location model and service

--- utmap-server/src/app/main/service/locationService.py
+++ utmap-server/src/app/main/service/locationService.py
@@ -15,15 +15,13 @@ def addLocation(data):
         return jsonify(responseObject, 409)
     else:
         newLocation = Location(
-            name=data['name'],
-            code=data['code'],
-            x=data['coords'][0],
-            y=data['coords'][1],
-            subLocations=data['subLocations']
+            name=data['name'], code=data['code'],x=data['coords'][0], 
+            y=data['coords'][1], subLocations=data['subLocations']
         )
         newId = newLocation.assignLocationId(locations)
         newLoc = location.findLocById(newId, locations)
-        responseBody = newLocation.formatAsResponseBody()
+        #newLocation.formatSublocations(newLoc)
+        responseBody = newLocation.formatAsResponseBody(newLoc)
         
         responseObject = {
             'status': 'success',
@@ -32,4 +30,54 @@ def addLocation(data):
         }
         return jsonify(responseObject, 201)
 
+def updateLocation(data):
+    pass
+
+def getAllLocations():
+    location = Location()
+    locations = location.connectToLocations()
+    responseBody = location.formatAllLocs(locations)
+
+    responseObject = {
+        'status': 'success',
+        'message': 'Found all locations',
+        'body' : responseBody
+        }
+    return jsonify(responseObject, 200)
+
+def getOneLocation(_id):
+    location = Location()
+    locations = location.connectToLocations()
+    loc = location.findLocById(_id, locations)
+    if loc:
+        responseBody = location.formatOneLoc(loc)
+        responseObject = {
+        'status': 'success',
+        'message': 'Found one location',
+        'body' : responseBody
+        }
+        return jsonify(responseObject, 200)
+    else:
+        responseObject = {
+        'status': 'fail',
+        'message': 'Location not found',
+        'body' : responseBody
+        }
+        return jsonify(responseObject, 404)
+
+def deleteOneLocation(_id):
+    location = Location()
+    locations = location.connectToLocations()
+    if location.deleteLocById(_id, locations):
+        responseObject = {
+            'status': 'success',
+            'message': 'Location successfully deleted'
+        }
+        return jsonify(responseObject, 200)
+    else:
+        responseObject = {
+            'status': 'fail',
+            'message': 'Location not found'
+        }
+        return jsonify(responseObject, 404)
     
