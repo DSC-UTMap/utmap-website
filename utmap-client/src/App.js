db095c1d4a4729f5b3740dc8ffb33fb86f0d66fa
Leila-U <leila.uy@mail.utoronto.ca>
2021-01-31 21:17:03 -0500

    Implement single event page

--- utmap-client/src/App.js
+++ utmap-client/src/App.js
@@ -6,6 +6,24 @@ import MapPage from './components/pages/MapPage';
 import CreateEventPage from './components/pages/CreateEventPage';
 import SingleEventPage from "./components/pages/SingleEventPage";
 
+//example event for single event page. REMOVE once single event connected to calendar
+const exampleEvents =
+  { startDate: '2021-01-03T09:45', 
+    endDate: '2021-01-03T10:00', 
+    title: 'Cry and Code',  
+    description: `Ever feel so overwhelmed that you wanna break down. Well in this event, 
+    you can do that and more. We rent our couches so you can cry on a comfortable surface. 
+    It's okay if you just want to chill and cry with friends or use this time to improve
+    your CS skills. As a pro-coder and crier, I use this event everyday. Sometimes we allow
+    students to book their own room in advanced so if you wanted to blast Comftoratably
+    Numb, we got you! Maybe your dog died or your partner broke up with you but you got an
+    Assignment due in 12 hours. Don't worry we got professional yellers to motivate you!
+    They will come into your private room and yell at you while you cry.`,
+    location: 'Davis',
+    sublocation: 'Spiegal Hall',
+    organizer: 'Qianqian Feng'
+  }
+
 function App() {
   return(
     //Landing, Calendar, and Map Page
@@ -15,8 +33,22 @@ function App() {
         <Route path="/calendar" component={CalendarPage}/>
         <Route path="/map" component={MapPage}/>
         <Route path="/createEvent" component={CreateEventPage}/>
-        <Route path="/singleEvent" component={SingleEventPage}/> 
-
+        {/* single event. REMOVE route when conncted to calendar */}
+        <Route 
+          path="/singleEvent" 
+          render={props => (
+            <React.Fragment>
+              <SingleEventPage 
+              startDate={exampleEvents.startDate} 
+              endDate={exampleEvents.endDate} 
+              title ={exampleEvents.title} 
+              description={exampleEvents.description}
+              location={exampleEvents.location}
+              sublocation={exampleEvents.sublocation}
+              organizer={exampleEvents.organizer}
+              />
+            </React.Fragment>
+          )}/> 
         <div><Link to="/">Landing</Link></div>
         <div><Link to="/calendar">Calendar</Link></div>
         <div><Link to="/map">Map</Link></div>
--- utmap-client/src/components/pages/SingleEventPage.js
+++ utmap-client/src/components/pages/SingleEventPage.js
@@ -1,42 +1,148 @@
 import React from 'react';
 import {
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
-    Paper
+	Avatar,
+	Collapse,
+	IconButton,
+	Grid,
+	GridList,
+	makeStyles,
+	Paper,
+	Typography,
 } from '@material-ui/core';
-import Draggable from 'react-draggable';
-/*
-const useStyles = makeStyles(theme => ({
-    box: {
-        display: 'flex',
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import CloseIcon from '@material-ui/icons/Close';
+import EditIcon from '@material-ui/icons/Edit';
+import PropTypes from 'prop-types';
 
-    }
-}))
-*/
-function SingleEventPage() {
+const useStyles = makeStyles(theme => ({ //CSS styles on components
+	box: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'gray',
+	},
+	paper: {
+		width: '35rem',
+		marginTop: theme.spacing(6),
+		paddingTop: theme.spacing(4),
+		paddingBottom: theme.spacing(4),
+		paddingLeft: theme.spacing(4),
+		paddingRight: theme.spacing(4),
+		display: 'flex',
+	},
+	largeIcon: {
+		width: theme.spacing(6),
+		height: theme.spacing(6),
+	},
+	largeAvatar: {
+		background: '#002884',
+		width: theme.spacing(10),
+		height: theme.spacing(10),
+	},
+	row: {
+		width: '100%',
+		display: 'flex',
+		alignItems: 'center',
+	},
+	title: {
+		textAlign: 'center',
+	},
+
+}));
+
+function SingleEventPage({startDate, endDate, title, description, location, sublocation, organizer}) {
+	const classes = useStyles();
+	
+	//strings for the dates and location
+	const startDateString = "Start Date: " + (new Date(startDate)).toLocaleString()
+	const endDateString = "End Date: " + (new Date(endDate)).toLocaleString()
+	const locationString = sublocation + " in " + location
+
+	//expand for description
+	const [expanded, setExpanded] = React.useState(false);
+  	const handleExpandClick = () => {
+    	setExpanded(!expanded);
+	  };
+	  
     return (
-        <div>
-            <Dialog
-                //PaperComponent={PaperComponent}
-                aria-labelledby="draggable-dialog-title"
-            >
-            {/*Event Title*/}
-                <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
-                    Event Title
-                </DialogTitle>
-                {/*Event Information*/}
-                <DialogContent>
-                    <DialogContentText>
-                        Event Information
-                    </DialogContentText>
-                </DialogContent>
-            </Dialog>
+        <div className={classes.box}>
+            <Paper className={classes.paper}>
+				<GridList cellHeight='auto' cols={1}>
+
+					{/* Close and Edit Icons */}
+					<Grid item className={classes.row}>
+						<Grid item xs="6" align="left">
+							<IconButton aria-label="close"><CloseIcon /></IconButton>
+						</Grid>
+						<Grid item xs="6" align="right">
+							<IconButton aria-label="edit"><EditIcon /></IconButton>
+						</Grid>
+					</Grid>
+
+					{/* Event Icon */}
+					<div align="center">
+						<Avatar className={classes.largeAvatar}>
+							<AssignmentIcon className={classes.largeIcon}/>
+						</Avatar>
+					</div>
+
+					{/* Title */}
+					<Typography className={classes.title} variant="h4">
+						{title}
+					</Typography>
+
+					{/* Organizer */}
+					<Typography variant="caption" align="center">
+						Organizer: {organizer}
+					</Typography>
+
+					{/* Location */}
+					<Typography variant="subtitle1" align="center">
+						{locationString}
+					</Typography>
+
+					{/* Dates and Times */}
+					<Grid item className={classes.row}>
+						<Grid item xs="6" align="center">
+							<Typography variant="subtitle2" align="center">
+								{startDateString}
+							</Typography>
+						</Grid>
+						<Grid item xs="6" align="center">
+							<Typography variant="subtitle2" align="center">
+								{endDateString}
+							</Typography>
+						</Grid>
+					</Grid>
+
+					{/* Description */}
+					<div align="right">
+						<IconButton onClick={handleExpandClick} aria-label="show more">
+							{expanded ? <ExpandLessIcon/> : <ExpandMoreIcon/>}
+						</IconButton>
+					</div>
+					<Collapse in={expanded} timeout="auto" unmountOnExit>
+						<Typography variant="body2" component="span">
+							Description: {description}
+						</Typography>
+					</Collapse>
+
+				</GridList>
+            </Paper>
         </div>
     );
+}
+
+SingleEventPage.propTypes = {
+	startDate: PropTypes.string.isRequired,
+	endDate: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+	location: PropTypes.string.isRequired,
+	sublocation: PropTypes.string.isRequired,
+	organizer: PropTypes.string.isRequired,
 }
  
 export default SingleEventPage;
