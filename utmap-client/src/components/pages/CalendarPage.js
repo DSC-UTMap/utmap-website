977e858f916af3ce8f27e3dab20a67a9ee51d5d2
Kana Hashimoto <kana.hashimoto@mail.utoronto.ca>
2021-01-31 21:04:36 -0500

    Add interactivity to events on calendar

--- utmap-client/src/components/pages/CalendarPage.js
+++ utmap-client/src/components/pages/CalendarPage.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import Paper from '@material-ui/core/Paper';
+import Dialog from '@material-ui/core/Dialog';
 import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
@@ -17,20 +18,50 @@ const exampleEvents = [
     { startDate: '2021-03-27T12:00', endDate: '2021-03-30T13:30', title: 'Get jacked' }
 ]
 
+
 //Scheduler is the calendar, today, and taskbar components
- function CalendarPage() {    
-    return (
-        <Paper>
-            <Scheduler data={exampleEvents}>
-            <ViewState />
-            <MonthView />
-            <Toolbar />
-            <DateNavigator />
-            <TodayButton />
-            <Appointments />
-            </Scheduler>
-        </Paper>
-    );    
+function CalendarPage() { 
+	const [openEventInfo, setOpenEventInfo] = useState(false);
+
+	const handleOpenEventInfo = () => {
+		setOpenEventInfo(true);
+	}
+	const handleCloseEventInfo = () => {
+		setOpenEventInfo(false);
+	}
+
+	const CustomActionAppointment = (props) => {
+		return (
+			<Appointments.Appointment
+				{...props}
+				onClick={handleOpenEventInfo}
+			/>
+		);
+	}
+
+	return (
+		<>
+			<Paper>
+			  <Scheduler data={exampleEvents}>
+			  <ViewState />
+			  <MonthView />
+			  <Toolbar />
+			  <DateNavigator />
+			  <TodayButton />
+			  <Appointments
+					appointmentComponent={CustomActionAppointment}
+				/>
+			  </Scheduler>
+			</Paper>
+			{/* Popup box for event info */}
+			<Dialog open={openEventInfo} onClose={handleCloseEventInfo}>
+				{/* Placeholder for event popup box*/}
+				<Paper>
+					<p>Event information here</p>
+				</Paper>
+			</Dialog>
+		</>
+	);    
 }
 
 export default CalendarPage;
