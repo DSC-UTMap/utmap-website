8042c282362d75439bb281c4c0bdc7e5fb6a2ca1
Crawsoir <werq10.fifty@gmail.com>
2021-02-14 20:26:15 -0500

    Add fade in/out to description

--- utmap-client/src/components/EventListItem.js
+++ utmap-client/src/components/EventListItem.js
@@ -6,58 +6,105 @@ import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { withStyles } from '@material-ui/core/styles';
 import Divider from '@material-ui/core/Divider';
+import Fade from '@material-ui/core/Fade';
 import PropTypes from 'prop-types';
 
 
 const styles = theme => ({
     column: {
-        flexBasis: '33.33%',
+        flexBasis: '20%',
     },
     heading: {
         fontSize: theme.typography.pxToRem(15),
-        flexShrink: 0,
-      },
+        flexBasis: '40%',
+    },
     secondaryHeading: {
         fontSize: theme.typography.pxToRem(15),
+        flexBasis: '40%',
         color: theme.palette.text.secondary,
     },
+    description: {
+        width: '100%',
+    }
   });
 
 export class EventListItem extends Component {
+    constructor() {
+        super();
+        this.onAccordionClick = this.onAccordionClick.bind(this);
+        this.state = { expanded: true };
+    }
+
+    onAccordionClick(){
+        this.setState({
+            expanded: !this.state.expanded
+        });
+    }
+
+    displayTitle(){
+        if (this.props.event.title.length >= 50){
+            return (this.props.event.title.substring(0,50) + '...');
+        } else {
+            return (this.props.event.title);
+        }
+    }
+
+    displayDescription(){
+        if (this.props.event.description.length >= 50){
+            return (this.props.event.description.substring(0,50) + '...');
+        } else {
+            return (this.props.event.description);
+        }
+    }
+
+    displayDate(){
+        var date = new Date(this.props.event.startDate);
+        return (date.toDateString());
+    }
 
     render() {
         const { classes } = this.props;
-        var date = new Date(this.props.event.startDate);
+        
         return (
-            <div>
-                <Accordion>
-                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                        {/* Event Title and First 50 characters */}
-                        <div className={classes.column}>
-                            <Typography className={classes.heading}>{this.props.event.title}</Typography>
-                        </div>
-                        <div className={classes.column}>
-                            <Typography className={classes.secondaryHeading} >
-                                {this.props.event.description.substring(0,50)}
+            <Accordion onChange={this.onAccordionClick}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    {/* Event Title and First 50 characters */}
+                    <div className={classes.heading}>
+                        <Typography>
+                            {this.displayTitle()}
+                        </Typography>
+                    </div>
+                    <div className={classes.secondaryHeading}>
+                        <Fade in={this.state.expanded}>
+                            <Typography>
+                                {this.displayDescription()}
                             </Typography>
-                        </div>
-                        {/* Event Date and Location*/}
-                        <div className={classes.column}>
-                            <Typography variant="caption" noWrap='true' >
-                                {date.toDateString()}
-                            </Typography>
-                            <Divider />
-                            <Typography variant="caption" noWrap='true' >
-                                {this.props.event.location}
-                            </Typography>
-                        </div>
-                    </AccordionSummary>
-                    <AccordionDetails>
+                        </Fade>
+                    </div>
+                    {/* Event Date and Location*/}
+                    <div className={classes.column}>
+                        <Typography variant="caption" noWrap='true' >
+                            {this.displayDate()}
+                        </Typography>
+                        <Divider />
+                        <Typography variant="caption" noWrap='true' >
+                            {this.props.event.location}
+                        </Typography>
+                    </div>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <div className={classes.description}>
                         {/* Event Description */}
-                        <Typography variant="body1">{this.props.event.description}</Typography>
-                    </AccordionDetails>
-                </Accordion>
-            </div>
+                        <Typography align='center' variant='h6'>
+                            {this.props.event.title}
+                        </Typography>
+                        <Typography>
+                            {this.props.event.description}
+                        </Typography>
+                    </div>
+
+                </AccordionDetails>
+            </Accordion>
         )
     }
 }
