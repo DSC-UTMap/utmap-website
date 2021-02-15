5f8ecc3635d43bf14574cc0abaf6f050e6d2bf0a
Crawsoir <werq10.fifty@gmail.com>
2021-02-15 15:25:41 -0500

    Convert eventlist to functional components

--- utmap-client/src/components/EventList.js
+++ utmap-client/src/components/EventList.js
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 import EventListItem from './EventListItem';
 
-export class EventList extends Component {
-
-  render() {
-      return (
-          <div>
-              <Typography variant="h5" gutterBottom>Upcoming Events</Typography>
-              {/* Display an Accordion for each item in eventList */}
-              {this.props.eventList.map((event) => (<EventListItem event={event}/>))}
-          </div>
-      )
-    }
+function EventList (props) {
+  return (
+      <div>
+          <Typography variant="h5" gutterBottom>Upcoming Events</Typography>
+          {/* Display an Accordion for each item in eventList */}
+          {props.eventList.map((event) => (<EventListItem event={event}/>))}
+      </div>
+  )
 }
 
 // PropTypes
--- utmap-client/src/components/EventListItem.js
+++ utmap-client/src/components/EventListItem.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Divider from '@material-ui/core/Divider';
 import Fade from '@material-ui/core/Fade';
 import PropTypes from 'prop-types';
 
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
     column: {
         flexBasis: '20%',
     },
@@ -26,93 +26,84 @@ const styles = theme => ({
     description: {
         width: '100%',
     }
-  });
+}));
 
-export class EventListItem extends Component {
-    constructor() {
-        super();
-        this.onAccordionClick = this.onAccordionClick.bind(this);
-        this.state = { expanded: true };
+function EventListItem(props) {
+    const classes = useStyles();
+    const [expanded, setExpanded] = useState(false);
+
+    const onAccordionClick = () => {
+        setExpanded(!expanded);
     }
 
-    onAccordionClick(){
-        this.setState({
-            expanded: !this.state.expanded
-        });
-    }
-
-    displayTitle(){
-        if (this.props.event.title.length >= 50){
-            return (this.props.event.title.substring(0,50) + '...');
+    const displayTitle = () => {
+        if (props.event.title.length >= 50){
+            return (props.event.title.substring(0,50) + '...');
         } else {
-            return (this.props.event.title);
+            return (props.event.title);
         }
     }
 
-    displayDescription(){
-        if (this.props.event.description.length >= 50){
-            return (this.props.event.description.substring(0,50) + '...');
+    const displayDescription = () => {
+        if (props.event.description.length >= 50){
+            return (props.event.description.substring(0,50) + '...');
         } else {
-            return (this.props.event.description);
+            return (props.event.description);
         }
     }
 
-    displayDate(){
-        var date = new Date(this.props.event.startDate);
+    const displayDate = () => {
+        var date = new Date(props.event.startDate);
         return (date.toDateString());
     }
-
-    render() {
-        const { classes } = this.props;
         
-        return (
-            <Accordion onChange={this.onAccordionClick}>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    {/* Event Title and First 50 characters */}
-                    <div className={classes.heading}>
+    return (
+        <Accordion onChange={onAccordionClick}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                {/* Event Title and First 50 characters */}
+                <div className={classes.heading}>
+                    <Typography>
+                        {displayTitle()}
+                    </Typography>
+                </div>
+                <div className={classes.secondaryHeading}>
+                    <Fade in={!expanded}>
                         <Typography>
-                            {this.displayTitle()}
+                            {displayDescription()}
                         </Typography>
-                    </div>
-                    <div className={classes.secondaryHeading}>
-                        <Fade in={this.state.expanded}>
-                            <Typography>
-                                {this.displayDescription()}
-                            </Typography>
-                        </Fade>
-                    </div>
-                    {/* Event Date and Location*/}
-                    <div className={classes.column}>
-                        <Typography variant="caption" noWrap='true' >
-                            {this.displayDate()}
-                        </Typography>
-                        <Divider />
-                        <Typography variant="caption" noWrap='true' >
-                            {this.props.event.location}
-                        </Typography>
-                    </div>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <div className={classes.description}>
-                        {/* Event Description */}
-                        <Typography align='center' variant='h6'>
-                            {this.props.event.title}
-                        </Typography>
-                        <Typography>
-                            {this.props.event.description}
-                        </Typography>
-                    </div>
+                    </Fade>
+                </div>
+                {/* Event Date and Location*/}
+                <div className={classes.column}>
+                    <Typography variant="caption" noWrap='true' >
+                        {displayDate()}
+                    </Typography>
+                    <Divider />
+                    <Typography variant="caption" noWrap='true' >
+                        {props.event.location}
+                    </Typography>
+                </div>
+            </AccordionSummary>
+            <AccordionDetails>
+                <div className={classes.description}>
+                    {/* Event Description */}
+                    <Typography align='center' variant='h6'>
+                        {props.event.title}
+                    </Typography>
+                    <Typography>
+                        {props.event.description}
+                    </Typography>
+                </div>
 
-                </AccordionDetails>
-            </Accordion>
-        )
-    }
+            </AccordionDetails>
+        </Accordion>
+    )
+    
 }
 
 // PropTypes
 EventListItem.propTypes = {
     event: PropTypes.object.isRequired,
-    classes: PropTypes.object.isRequired
 }
 
-export default withStyles(styles)(EventListItem);
+export default EventListItem;
