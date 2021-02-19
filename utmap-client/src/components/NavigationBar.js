2ad550a66fc24808d86ef37eb94ed49c1caeb280
Kana Hashimoto <kana.hashimoto@mail.utoronto.ca>
2021-02-18 21:45:01 -0500

    Add styling to SideBar

--- utmap-client/src/components/NavigationBar.js
+++ utmap-client/src/components/NavigationBar.js
@@ -9,17 +9,17 @@ import EventIcon from '@material-ui/icons/Event';
 
 function NavigationBar() {
 	return (
-		<>
+		<div style={{width:'100%'}}>
 			<Button variant='outlined'>
 				<LocationOnIcon />
 			</Button>
 			<Button variant='outlined'>
 				<EventIcon />
 			</Button>
-			<Button variant='outlined'>
+			<Button variant='outlined' style={{float:'right'}}>
 				About
 			</Button>
-		</>
+		</div>
 	);
 }
 
