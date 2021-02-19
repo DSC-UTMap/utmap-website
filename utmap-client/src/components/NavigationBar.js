3a2397a3c87d4cdfba8c299bb956182ad25feff1
Kana Hashimoto <kana.hashimoto@mail.utoronto.ca>
2021-02-18 22:42:46 -0500

    Delete temp files and make Navbar functional

--- utmap-client/src/components/NavigationBar.js
+++ utmap-client/src/components/NavigationBar.js
@@ -1,22 +1,43 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import CalendarPage from './pages/CalendarPage';
-import MapPage from './pages/MapPage';
+import {Link, useLocation} from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import EventIcon from '@material-ui/icons/Event';
+import { makeStyles } from '@material-ui/core/styles';
+import indigo from '@material-ui/core/colors/indigo';
+
+const useStyles = makeStyles((theme) => ({
+  normal : {
+		background: 'white'
+	},
+	highlight : {
+		background: indigo[100],
+		'&:hover': {
+			background: indigo[100],
+		}
+	},
+}));
 
 
 function NavigationBar() {
+	const style = useStyles();
+	const path = useLocation().pathname;
+
 	return (
 		<div style={{width:'100%'}}>
-			<Button variant='outlined'>
+			<Button 
+				className={path === '/map' ? style.highlight : style.normal} 
+				variant='outlined' component={Link} to='/map'
+			>
 				<LocationOnIcon />
 			</Button>
-			<Button variant='outlined'>
+			<Button 
+				className={path === '/calendar' ? style.highlight : style.normal} 
+				variant='outlined' component={Link} to='/calendar'
+			>
 				<EventIcon />
 			</Button>
-			<Button variant='outlined' style={{float:'right'}}>
+			<Button variant='outlined' style={{float:'right'}} component={Link} to='/'>
 				About
 			</Button>
 		</div>
