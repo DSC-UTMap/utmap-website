0552596ca82429ac8447d6130b84151547b65786
Kana Hashimoto <kana.hashimoto@mail.utoronto.ca>
2021-02-18 22:42:46 -0500

    Delete temp files and make Navbar functional

--- utmap-client/src/App.js
+++ utmap-client/src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import LandingPage from './components/pages/LandingPage';
 import CalendarPage from './components/pages/CalendarPage';
 import MapPage from './components/pages/MapPage';
-import EventListPage from './components/pages/EventListPage'
 
 function App() {
   return(
@@ -12,13 +11,10 @@ function App() {
         <Route exact path="/" component={LandingPage}/>    
         <Route path="/calendar" component={CalendarPage}/>
         <Route path="/map" component={MapPage}/>
-        {/* event list example page. Remove route when added to sidebar */}
-        <Route path="/eventlist" component={EventListPage}/>
     
         <div><Link to="/">Landing</Link></div>
         <div><Link to="/calendar">Calendar</Link></div>
         <div><Link to="/map">Map</Link></div>
-        <div><Link to="/eventlist">Event List</Link></div>
       </div>
     </Router>         
   );
