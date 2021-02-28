4e2f8b4a15a744365de86d2eecca919a8896e830
Crawsoir <werq10.fifty@gmail.com>
2021-02-27 19:03:27 -0500

    Implement landing page footer

---
+++ utmap-client/src/components/data/Contributors.js
@@ -0,0 +1,40 @@
+import a_bergen from "../img/a_bergen.png";
+import s_shahid from "../img/s_shahid.png";
+import l_uy from "../img/l_uy.png";
+import q_feng from "../img/q_feng.png";
+import k_hashimoto from "../img/k_hashimoto.png";
+import r_sanii from "../img/r_sanii.png";
+
+const avatars = [
+    {
+        url: a_bergen,
+        name: "Andi Bergen",
+        title: "Supervisor",
+    },
+    {
+        url: s_shahid,
+        name: "Shahmeer Shahid",
+        title: "Mentor",
+    },
+    {
+        url: l_uy,
+        name: "Leila Uy",
+        title: "Team Member",
+    },
+    {
+        url: q_feng,
+        name: "Qianqian Feng",
+        title: "Team Member",
+    },
+    {
+        url: k_hashimoto,
+        name: "Kana Hashimoto",
+        title: "Team Member",
+    },
+    {
+        url: r_sanii,
+        name: "Roberti Sanii",
+        title: "Team Member",
+    },
+]
+export default avatars;
