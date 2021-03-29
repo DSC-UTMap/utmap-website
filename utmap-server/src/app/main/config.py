81ee3596cc900ae42d4c1b6ae1c37474cad9a2a5
Crawsoir <werq10.fifty@gmail.com>
2021-03-29 00:48:23 -0400

    Add environment variable for database init

--- utmap-server/src/app/main/config.py
+++ utmap-server/src/app/main/config.py
@@ -6,6 +6,7 @@ class Config:
     SECRET_KEY = os.getenv('SECRET KEY', 'skeleton_key_sans_undertale_haha')
     MONGODB_HOST = os.getenv('MONGODB_HOST', 'localhost')
     MONGODB_PORT = int(os.getenv('MONGODB_PORT', 27017))
+    SKIP_DB_INIT = os.getenv('SKIP_DB_INIT', False)
     SERVER_HOST = os.getenv('SERVER_HOST', '127.0.0.0')
     SERVER_PORT = int(os.getenv('SERVER_PORT', 5000))
     DEBUG = bool(os.getenv('SERVER_PORT', False))
@@ -38,6 +39,7 @@ key = Config.SECRET_KEY
 
 dbHost = Config.MONGODB_HOST
 dbPort = Config.MONGODB_PORT
+skipDBInit = Config.SKIP_DB_INIT
 serverHost = Config.SERVER_HOST
 serverPort = Config.SERVER_PORT
 debugSetting = Config.DEBUG
