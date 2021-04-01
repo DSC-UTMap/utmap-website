449235909d6613f76fb8e4048bdd3f3d98d67571
Crawsoir <werq10.fifty@gmail.com>
2021-03-31 20:05:46 -0400

    Correctly convert environment vars into booleans

--- utmap-server/src/app/main/config.py
+++ utmap-server/src/app/main/config.py
@@ -6,10 +6,10 @@ class Config:
     SECRET_KEY = os.getenv('SECRET KEY', 'skeleton_key_sans_undertale_haha')
     MONGODB_HOST = os.getenv('MONGODB_HOST', 'localhost')
     MONGODB_PORT = int(os.getenv('MONGODB_PORT', 27017))
-    SKIP_DB_INIT = os.getenv('SKIP_DB_INIT', False)
+    SKIP_DB_INIT = os.getenv('SKIP_DB_INIT', 'False') == 'True'
     SERVER_HOST = os.getenv('SERVER_HOST', '127.0.0.0')
     SERVER_PORT = int(os.getenv('SERVER_PORT', 5000))
-    DEBUG = bool(os.getenv('SERVER_PORT', False))
+    DEBUG = os.getenv('DEBUG', 'False') == 'True'
     BUILDINGS = os.getenv('BUILDINGS', 'building_config.json')
 
 
