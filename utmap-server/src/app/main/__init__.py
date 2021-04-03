b4023ed76a2d2b9cb97952fe72f05cd660a0e6a9
Crawsoir <werq10.fifty@gmail.com>
2021-04-02 23:16:49 -0400

    Use variables for database host and name

--- utmap-server/src/app/main/__init__.py
+++ utmap-server/src/app/main/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from pymongo import MongoClient
 from .config import configByName, dbHost, dbPort, dbUser, dbPassword, dbName, serverHost, serverPort, debugSetting, skipDBInit
 
-db_connect_string = "mongodb+srv://{user}:{password}@cluster0.slrlz.mongodb.net/UTMap?retryWrites=true&w=majority".format(user=dbUser, password=dbPassword)
+db_connect_string = "mongodb+srv://{user}:{password}@{dbHost}/{dbName}?retryWrites=true&w=majority".format(user=dbUser, password=dbPassword, dbHost=dbHost, dbName=dbName)
 
 db = MongoClient(db_connect_string).get_database(dbName)
 
